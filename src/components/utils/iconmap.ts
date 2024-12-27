e177e9ee53e173c7adc5cd2d963663ef484650f9
Bradley Jones <pgawestjones@gmail.com>
2024-12-27 15:03:08 -0800

    Added icons for new apps

--- src/components/utils/iconmap.ts
+++ src/components/utils/iconmap.ts
@@ -8,6 +8,8 @@ import {
   Code,
   Settings,
   Bot,
+  Blocks,
+  Grid2x2X,
   AppWindow, // Added as another fallback
 } from 'lucide-react';
 
@@ -22,6 +24,8 @@ export const iconMap = {
   settings: Settings,
   bot: Bot,
   messagesquare: MessageSquare,
+  blocks: Blocks,
+  tictactoe: Grid2x2X,
   app: AppWindow, // Added as default app icon
 };
 
