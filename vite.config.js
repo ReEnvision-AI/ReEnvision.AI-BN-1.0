2c1c52c183be626f6a582e58d3a48fd17319d1bc
ReEnvisionAi <168676352+ReEnvisionAi@users.noreply.github.com>
2025-04-02 20:59:08 -0700

    added _redirects

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import reactNativeWeb from 'vite-plugin-react-native-web';
-import mkcert from 'vite-plugin-mkcert'
+
 
 export default defineConfig({
-  plugins: [reactNativeWeb(), react(), mkcert()],
+  plugins: [reactNativeWeb(), react()],
   resolve: {
     extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json']
   },
